c8de4245002bdc745e9d02bcca93b1fb4a0954a4
lenboy11 <lennartherr@gmx.de>
2021-09-09 02:39:33 +0200

    Clearifying UnicodeEncodeError

--- ScrapeAccenture.py
+++ ScrapeAccenture.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
-import os, sys
 
 def main():
     # Initial Values 
@@ -14,9 +13,11 @@ def main():
     cs = {}
     try:
         for country in countrylist.find_all(class_='list-group-item'):
-            countryCode = country['data-country-site'] # Country Code, e.g. za-en (South Africa - English)
+            countryLanguageCode = country['data-country-site'] # Country Code, e.g. za-en (South Africa - English)
+            languageCode = country['data-page-language']
+            countryCode = country['value']
             # Determine Language (English, German, Spanish, ...)
-            textlist = country.text.split() # contains "countryName   ( Language )"  where the spacing is uncommon and country name can have spaces, e.g. South Africa
+            textlist = country.text.split() # contains "countryName   ( languageName )"  where the spacing is uncommon and country name can have spaces, e.g. South Africa
             indexl = 1
             indexr = 4
             for i in range(2,len(textlist)):
@@ -25,12 +26,11 @@ def main():
                 elif textlist[i] == ")":
                     indexr = i
                     break
-            countryName = ' '.join(textlist[:indexl])
-            language = ' '.join(textlist[indexl+1:indexr])
-            # Filter by languages german (de) and english (en)
-            cs[countryCode] = [ countryCode[3:5] , countryName, language ] # Key: za-en, Values: en (for API), South Africa (for UI), English (for UI)
-    except Exception:
-        pass
+            countryName = ' '.join(textlist[:indexl])   # country['value'] might be in non latin-letters
+            languageName = ' '.join(textlist[indexl+1:indexr])
+            cs[countryLanguageCode] = [ countryCode, languageCode, countryName, languageName ] # Example -> Key: cn-zh | Values: 中国大陆 (for API), zh-cn (for API), China/Mainland (for UI), Chinese (for UI)
+    except Exception as e:
+        print("Exception while fetching countryCodes and languageCodes: " + str(e))
     # Second Part: Get Jobs for each country
     url = "https://www.accenture.com/api/sitecore/JobSearch/FindJobs"
     headers = {
@@ -46,35 +46,36 @@ def main():
         "x-requested-with": "XMLHttpRequest"
         }
     # Save results in csv file
-    csv_file = open('accenture_scrape.csv', 'w', encoding='UTF-8')
+    csv_file = open('accenture_scrape.csv', 'w', encoding='UTF-8', newline='')
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['Country', 'Job-Description', 'Link'])
-    for countryCode in cs:
+    csv_writer.writerow(['Country', 'Language', 'Job-Description', 'Link'])
+    for code in cs:
         # Payload holds the search values (Entry Level Jobs)
-        payload = "{\"f\":1,\"s\":9,\"k\":\"" + searchTerm + "\",\"lang\":\"" + cs[countryCode][0] + "\",\"cs\":\"" + countryCode + "\",\"df\":\"[{\\\"metadatafieldname\\\":\\\"skill\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"location\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"postedDate\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"travelPercentage\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"jobTypeDescription\\\",\\\"items\\\":[{\\\"term\\\":\\\"entry-level job\\\",\\\"selected\\\":true}]},{\\\"metadatafieldname\\\":\\\"businessArea\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"specialization\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"workforceEntity\\\",\\\"items\\\":[]}]\",\"c\":\"" + cs[countryCode][1] + "\",\"sf\":0,\"syn\":false,\"isPk\":false,\"wordDistance\":0,\"userId\":\"\"}"
-        #                 ?       ?             searchTerm (consultant)         Language (en)                         countryCode (us-en)                                   Name of the field: Selections                               Name of the field: Selections                                 Name of the field:   Selections                                     Name of the field:     Selections                                      Name of the field: Selections                     Entry-Level Job is selected                                                      Name of the field: Selections                                     Name of the field:   Selections                                    Name of the field:     Selections                     countryName (Deutschland)       ?           ?               ?                   ?             ?
+        payload = ("{\"f\":1,\"s\":9,\"k\":\"" + searchTerm + "\",\"lang\":\"" + cs[code][1] + "\",\"cs\":\"" + code + "\",\"df\":\"[{\\\"metadatafieldname\\\":\\\"skill\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"location\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"postedDate\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"travelPercentage\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"jobTypeDescription\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"businessArea\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"specialization\\\",\\\"items\\\":[]},{\\\"metadatafieldname\\\":\\\"workforceEntity\\\",\\\"items\\\":[]}]\",\"c\":\"" +cs[code][0].encode("utf8").decode("utf8") + "\",\"sf\":0,\"syn\":false,\"isPk\":false,\"wordDistance\":0,\"userId\":\"\"}")
+        #                 ?       ?             searchTerm (consultant)         languageCode (zh-cn)           countryLanguageCode (cn-zh)                                   Name of the field: Selections                               Name of the field: Selections                                 Name of the field:   Selections                                     Name of the field:     Selections                                      Name of the field: Selections       || For Entry-Level Jobs: {\\\"term\\\":\\\"entry-level job\\\",\\\"selected\\\":true}                 Name of the field: Selections                                     Name of the field:   Selections        countryCode (中国大陆)   ?         ?              ?                      ?            ?
+        #                 ?       ?             searchTerm (trainee)            languageCode (es)              countryLanguageCode (cl-es)                                   Name of the field: Selections                               Name of the field: Selections                                 Name of the field:   Selections                                     Name of the field:     Selections                                      Name of the field: Selections                             Name of the field: Selections                                                   Name of the field: Selections                                     Name of the field:   Selections        countryCode (Chile)     ?         ?              ?                      ?            ?
         # Get the Jobs Table
         try: 
             response = requests.request("POST", url, data=payload, headers=headers) # Issues with non latin letters like Chinese
+            dict = response.json()
         except Exception as e:
-            print( str(e) + str(str(cs[countryCode][1]).encode("utf-8")) )
-
-        dict = response.json()
+            dict = { 'documents' : [] }
+            print( "Exception while sending API Call: " + str(e) )
+        
         jobs = dict['documents']
         for job in jobs:
             # Add Country Code to jobUrl before adding it to the csv
             jobUrl = job['jobDetailUrl'].split('/')
-            jobUrl[3] = countryCode
+            jobUrl[3] = code
             try:
-                csv_writer.writerow([str(cs[countryCode][1]) + " (" + str(cs[countryCode][2]) + ")" , job['title'], '/'.join(jobUrl) ])
-            except Exception:
-                print(str(cs[countryCode][1]) + " (" + str(cs[countryCode][2]) + ") : " + job['title'] + " See: " + '/'.join(jobUrl))
+                csv_writer.writerow([cs[code][2], cs[code][3], job['title'], '/'.join(jobUrl) ])
+            except Exception as e:
+                print("Exception while writing in csv: " + str(e)) # Solved by opening with encoding='UTF-8'
 
     csv_file.close()
 
 if __name__=="__main__":
     main()
-
 
 # Payload in Detail:
 #  "{\"f\":1,
@@ -105,3 +106,29 @@ if __name__=="__main__":
 #   \"wordDistance\":0,
 #   \"userId\":\"\"}",
 
+#{\"f\":1,\"s\":9,
+# \"k\":\"train\",
+# \"lang\":\"zh-cn\",
+# \"cs\":\"cn-zh\",
+# \"df\":\"[{\\\"metadatafieldname\\\":\\\"skill\\\",
+#            \\\"items\\\":[]},
+#           {\\\"metadatafieldname\\\":\\\"location\\\",
+#            \\\"items\\\":[]},
+#           {\\\"metadatafieldname\\\":\\\"postedDate\\\",
+#            \\\"items\\\":[]},
+#           {\\\"metadatafieldname\\\":\\\"travelPercentage\\\",
+#            \\\"items\\\":[]},
+#           {\\\"metadatafieldname\\\":\\\"jobTypeDescription\\\",
+#            \\\"items\\\":[]},
+#           {\\\"metadatafieldname\\\":\\\"businessArea\\\",
+#            \\\"items\\\":[]},
+#           {\\\"metadatafieldname\\\":\\\"specialization\\\",
+#            \\\"items\\\":[]},
+#           {\\\"metadatafieldname\\\":\\\"workforceEntity\\\",
+#            \\\"items\\\":[]}]\",
+# \"c\":\"中国大陆\",
+# \"sf\":0,
+# \"syn\":false,
+# \"isPk\":false,
+# \"wordDistance\":0,
+# \"userId\":\"\"}",
